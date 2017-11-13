537cd2689e4f64a15320a7798e29db56e5ac2100
Zach Palumbo <zach@palumbo.io>
2017-11-13 13:39:58 -0500

    Better makesymlinks.sh script
    Signed-off-by: Zach Palumbo <zach@palumbo.io>

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,11 +9,16 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="arcrc bash_it bash_profile gitconfig hgrc vim vimrc"    # list of files/folders to symlink in homedir
+files="bash_it bash_profile gitconfig hgrc vim vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
+if [ -e $olddir ]; then
+    echo "Backup destination $olddir already exists."
+    echo "Please remove or rename it and try again."
+    exit 1
+fi
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
@@ -25,8 +30,9 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "Moving .$file from ~ to $olddir if it exists"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
