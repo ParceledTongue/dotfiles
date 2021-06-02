e652d6705d8658b283e44323d5572853176caff4
Zach Palumbo <zach@palumbo.io>
2021-06-02 14:43:54 -0400

    Rearrange dotfiles and rewrite makesymlinks script
    Signed-off-by: Zach Palumbo <zach@palumbo.io>

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
+
 ############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# From http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+# makesymlinks.sh
 ############################
 
-########## Variables
+backup=~/dotfiles_old
+repo_files=($(pwd)/files/*)
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_it bash_profile zshrc ghci gitconfig hgrc vim vimrc"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-if [ -e $olddir ]; then
-    echo "Backup destination $olddir already exists."
-    echo "Please remove or rename it and try again."
-    exit 1
+if [ -e $backup ]; then
+  echo "Backup destination $backup already exists."
+  echo "Please remove or rename it and try again."
+  exit 1
 fi
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating $backup for backup of any existing dotfiles in ~"
+mkdir -p $backup
 echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving .$file from ~ to $olddir if it exists"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+for repo_file in ${repo_files[@]}; do
+  fname=$(basename "$repo_file")
+  home_file=~/.$fname
+  if [ -e "$home_file" ]; then
+    echo "Moving $home_file to $backup."
+    mv "$home_file" $backup
+  fi
+  echo "Creating symlink for $fname."
+  ln -s "$repo_file" "$home_file"
 done
 
